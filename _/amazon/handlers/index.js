bb9e4e780107ca037d132a3f6f39bfd8b7819096
Kyle McNally <kyle@dazser.com>
2022-05-20 10:27:45 -0400

    Write a pdf artifact to check webgl

--- _/amazon/handlers/index.js
+++ _/amazon/handlers/index.js
@@ -1,6 +1,7 @@
 const { ok } = require('assert');
 const { createHash } = require('crypto');
 const chromium = require('@sparticuz/chrome-aws-lambda');
+const { writeFile } = require('fs');
 
 exports.handler = async (event, context) => {
   let browser = null;
@@ -36,6 +37,10 @@ exports.handler = async (event, context) => {
 
           if (job.expected.hasOwnProperty('screenshot') === true) {
             ok(createHash('sha1').update((await page.screenshot()).toString('base64')).digest('hex') === job.expected.screenshot, `Screenshot assertion failed.`);
+          }
+
+          if (job.expected.hasOwnProperty('pdf') === true) {
+            writeFile(job.expected.pdf, (await page.pdf())).then(ok());
           }
         }
       }
